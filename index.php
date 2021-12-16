c1e996ab82d551d31a9c237a1760c89834d134e0
IT20273408 <it20273408@my.sliit.lk>
2021-12-16 12:33:13 +0530

    adding css second

--- index.php
+++ index.php
@@ -8,6 +8,67 @@
     <link rel= "stylesheet" href="css/style.css"/>
 </head>
 <body>
+    <section class="main">
+       <nav>
+           <a href="#" class="logo"> 
+               <img src = "images/logo.jpg">
+               
+        </a>
+
+        <ul class= "menu">
+            <li> <a href= "#" class="active"> Home </a> </li>
+            <li> <a href= "#"> Features </a> </li>
+            <li> <a href= "#"> About </a> </li>
+            <li> <a href= "#"> Contract </a> </li>
+
+
+</ul>
     
+       
+    </nav>
+     <div class= "main-heading">
+         <h1> “Education is the most powerful weapon which you can use to change the world” </h1>
+
+         <p> Give to the needy today. The covid-19 pandemic has hit us hard than good and low income-families suffer most. Low-income families who are strongly to feed can not provide essential services to their families. Your gift today can help make a change to their families and their communities. Without the donations we received from December 2020 till date from individuals and corporation organizations donors, our ongoing back to school campaign could not be possible. The funds we received helped us to support over 476 unfortunate children's with school supplies, scholarships to orphans girls in secondary school and we were able to construct toilet blocks to end open defecation in the schools. We encourage you to continue supporting our initiatives to achieve more goals in various rural communities we serves. No amount is too small to support our missions.</p>
+         <a class= "main-btn" href="#"> Contact </a>
+</section>
+<section class="features">
+    <div class="feature-container">
+         
+        <div class="feature-box">
+            <div class="f-img">
+                <img src="images/info-icon1.png"/>
+            </div>
+            <div class = "f-text">
+    <h4> Web Development</h4>
+    <p> You might not have been born into the life you want to live forever. And that's where education can help. By getting an education, you give yourself the chance to change your life for the better. You might graduate and become a manager instead of an entry-level employee.</p>
+    <a href="#" class="main-btn"> Check  </a>
+</div>
+</div>
+
+<div class="feature-box">
+            <div class="f-img">
+                <img src="images/info-icon2.png"/>
+            </div>
+            <div class = "f-text">
+    <h4> Software Development</h4>
+    <p> You might not have been born into the life you want to live forever. And that's where education can help. By getting an education, you give yourself the chance to change your life for the better. You might graduate and become a manager instead of an entry-level employee.</p>
+    <a href="#" class="main-btn"> Check  </a>
+</div>
+</div>
+
+
+<div class="feature-box">
+            <div class="f-img">
+                <img src="images/info-icon3.png"/>
+            </div>
+            <div class = "f-text">
+    <h4> App Development</h4>
+    <p> You might not have been born into the life you want to live forever. And that's where education can help. By getting an education, you give yourself the chance to change your life for the better. You might graduate and become a manager instead of an entry-level employee.</p>
+    <a href="#" class="main-btn"> Check  </a>
+</div>
+</div>
+
+</div>  
 </body>
 </html>
